7e90cfc85ae91f77eb4078dcbe4c27556018f38c
Psanti Pineda <psantixt12@gmail.com>
2025-09-24 16:08:48 -0500

    HU00001: Registro de clientes y turnos terminados

---
+++ Cliente.java
@@ -0,0 +1,20 @@
+public class Cliente {
+    private int cedula;
+    private String nombre;
+    
+    public Cliente() {
+    }
+    public int getCedula() {
+        return cedula;
+    }
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
